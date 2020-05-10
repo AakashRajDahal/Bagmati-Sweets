2faa026e2e5142ddbbdb95dc57cb8393461d9f8d
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-05-10 09:58:07 +0545

    owl carousel added for hero slider

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// $('.navTrigger').click(function () {
+//     $(this).toggleClass('active');
+//     console.log("Clicked menu");
+//     $("#mainListDiv").toggleClass("show_list");
+//     $("#mainListDiv").fadeIn();
+  
+//   });
+
+  
+  $(document).ready(function(){
+    $('.banner-section').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrow:true,
+    });
+  });
+  

6163b4032a6c6c67debce38ee0fe7e765fd7632b
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-05-27 22:15:04 +0545

    _about-us.scss created and imported in style.scss

--- js/script.js
+++ js/script.js
@@ -4,18 +4,17 @@
 //     console.log("Clicked menu");
 //     $("#mainListDiv").toggleClass("show_list");
 //     $("#mainListDiv").fadeIn();
-  
+
 //   });
 
-  
-  $(document).ready(function(){
-    $('.banner-section').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-        arrow:true,
-    });
+
+$(document).ready(function () {
+  $('.banner-section').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrow: true,
   });
-  
+});
